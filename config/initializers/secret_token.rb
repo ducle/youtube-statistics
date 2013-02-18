ee1458c509a9a5ff81372b335521697faaa037db
Duc Le <ducle.sg@gmail.com>
2013-02-18 11:36:40 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YoutubeStatistics::Application.config.secret_token = 'ce0a1fde27fa322dbba34648534f89e21c831f74e96a8f4b18c85f2cc5daab4d6c136217064c36bd7a191c738ae406bea32a837961fd2a8423724a00b42be39b'
